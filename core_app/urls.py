ce60877d09536dde5ffd13f44194263130f22948
jhony <jhonyt37@gmail.com>
2015-02-25 01:46:25 -0500

    adicion de clsse correo para envio de mail

--- core_app/urls.py
+++ core_app/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 #from core_app import views
+from core_app import views
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     # ex: /app/
     url(r'^$', login_required(TemplateView.as_view(template_name='index.html'))),
+    # ex: /grupo/
+    url(r'^correo/$', login_required(views.HomeListView.as_view(template_name='app_list.html')), name='app_list'),
     #url(r'^$', login_required(views.HomeListView.as_view()), name='home_list'),                           
 )
--- core_app/views.py
+++ core_app/views.py
@@ -1,6 +1,6 @@
 #from django.shortcuts import render
 from django.views.generic import ListView
-
+from core_app.correo import  correo
 #==========================================================
 #Clase que gestiona los atributos requeridos por el home
 #==========================================================
@@ -8,5 +8,7 @@ class HomeListView(ListView):
     context_object_name = 'app_list' 
     
     def get_queryset(self):
+        c = correo.myCorreo()
+        c.enviarGmail()
         #user = self.request.user;
         return [];
