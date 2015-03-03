26601520f5f08a09d42eef6638861a98996b1300
jhony <jhonyt37@gmail.com>
2015-03-03 01:04:18 -0500

    adicion ambiente test

--- MySmartHome/settings.py
+++ MySmartHome/settings.py
@@ -68,11 +68,17 @@ WSGI_APPLICATION = 'MySmartHome.wsgi.application'
 #PWD_DB = 'UMq60bMuvQrXN0Ie9oJojMr5Yf'
 #HOST_DB = 'ec2-50-19-236-178.compute-1.amazonaws.com'
 
-#prod
-NAME_DB='dce330ch6019n5'
-USER_DB = 'jxkxkqtyokdkdf'
-PWD_DB = 'woo5BcTu62DbMZMvpRtf0KA7Li'
+#test
+NAME_DB='d7rtbk4m2c6c3k'
+USER_DB = 'qpdmestgkkdxhn'
+PWD_DB = 'dHC9ClLm8wyX7vt6U6ZiEjzOlo'
 HOST_DB = 'ec2-50-17-181-147.compute-1.amazonaws.com'
+
+#prod
+#NAME_DB='dce330ch6019n5'
+#USER_DB = 'jxkxkqtyokdkdf'
+#PWD_DB = 'woo5BcTu62DbMZMvpRtf0KA7Li'
+#HOST_DB = 'ec2-50-17-181-147.compute-1.amazonaws.com'
 
 
 DATABASES = {
