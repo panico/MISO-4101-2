6a0f75f0ca8df2962423d2d1faf269eaf1942a8a
jhony <jhonyt37@gmail.com>
2015-03-23 23:23:44 -0500

    ajuste statics heroku

--- MySmartHome/settings.py
+++ MySmartHome/settings.py
@@ -95,28 +95,39 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
+ 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ 
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+ 
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+ 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 #Custome template folder
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
 LOGIN_REDIRECT_URL='/'
 LOGIN_URL='/login/'
