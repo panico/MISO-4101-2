c4d936351f4543778d24294af11d466e198fff9b
Ger L <ing.rojas.m@gmail.com>
2015-04-07 19:55:58 -0500

    Adding files

--- MySmartHome/urls.py
+++ MySmartHome/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
-from django.conf import settings
+
 urlpatterns = patterns('',
     #url(r'^$', login_required(TemplateView.as_view(template_name='index.html'))),
     url(r'^$', RedirectView.as_view(url='/app/')),
@@ -9,7 +9,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout'),
-)
-urlpatterns += patterns('',
-	(r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
